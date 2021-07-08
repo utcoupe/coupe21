d585fc8077b45c2df712efe94d9c0154b706e38f
Villanut0 <villanueva.theo21@gmail.com>
2021-07-08 19:50:05 +0200

    Fix roslaunch script

--- scripts/roslaunch_start.sh
+++ scripts/roslaunch_start.sh
@@ -4,6 +4,6 @@ source ~/.bashrc
 source /opt/ros/melodic/setup.sh
 source $UTCOUPE_WORKSPACE/ros_ws/devel/setup.sh
 
-/usr/bin/python /opt/ros/melodic/bin/roslaunch definitions general.launch robot:=$1 &
-sleep 2
+/usr/bin/python /opt/ros/melodic/bin/roslaunch definitions general.launch robot:=$1 #&
+#sleep 2
 
